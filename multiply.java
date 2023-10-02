2cd733773c835c700aa90cb664251108adda78c2
Mayuresh Harihar <hariharmayuresh@gmail.com>
2023-10-02 08:06:45 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers to multiply
+        int num1 = 5;
+        int num2 = 7;
+
+        // Calculate the product
+        int product = num1 * num2;
+
+        // Display the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
